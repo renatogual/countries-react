564c3831368baf574a64a674e8e156cce70ef093
renatogual <renatogual@hotmail.com>
2021-04-06 15:01:02 -0300

    Importado componente card do material-ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import { Container, TextField } from '@material-ui/core/'
+import {
+  Container,
+  Divider,
+  Grid,
+  TextField,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+} from '@material-ui/core/'
 import { makeStyles } from '@material-ui/core/styles'
 
 import api from '../services/Api'
 
 const useStyles = makeStyles(theme => ({
-  root: {
+  input: {
     textAlign: 'center',
     '& > *': {
       margin: theme.spacing(5),
       width: '50%',
     },
+  },
+  grid: {
+    marginTop: theme.spacing(2),
+    flexGrow: 1,
+  },
+  media: {
+    height: 200,
   },
 }))
 
@@ -25,9 +42,31 @@ function Home() {
   console.log(data)
   return (
     <Container fixed>
-      <form className={classes.root} noValidate autoComplete="off">
+      <form className={classes.input} noValidate autoComplete="off">
         <TextField label="Buscar país" variant="outlined" />
       </form>
+      <Divider />
+      <Grid container className={classes.grid} spacing={4}>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="/static/images/cards/contemplative-reptile.jpg"
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Brazil
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
