b3edf6961d2587a796e78075cc30c6a427ce0bdc
renatogual <renatogual@hotmail.com>
2021-04-06 16:06:17 -0300

    Importado componente pagination do material-ui/lab

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import {
   Container,
@@ -11,6 +13,7 @@ import {
   Typography,
 } from '@material-ui/core/'
 import { makeStyles } from '@material-ui/core/styles'
+import Pagination from '@material-ui/lab/Pagination'
 
 import api from '../services/Api'
 
@@ -29,6 +32,12 @@ const useStyles = makeStyles(theme => ({
   media: {
     height: 200,
   },
+  pagination: {
+    margin: theme.spacing(5),
+    '& ul': {
+      justifyContent: 'center',
+    },
+  },
 }))
 
 function Home() {
@@ -38,35 +47,53 @@ function Home() {
   useEffect(() => {
     api.get('all').then(setData)
   }, [])
-
   console.log(data)
+  function createList() {
+    const list = []
+    for (let i = 0; i < 200; i++) {
+      list.push(i)
+    }
+    return list
+  }
+
+  const lista = createList()
+
   return (
     <Container fixed>
       <form className={classes.input} noValidate autoComplete="off">
-        <TextField label="Buscar país" variant="outlined" />
+        <TextField label="Buscar país" variant="outlined" color="secondary" />
       </form>
       <Divider />
+
       <Grid container className={classes.grid} spacing={4}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Card>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="/static/images/cards/contemplative-reptile.jpg"
-                title="Contemplative Reptile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Brazil
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+        {lista.map(item => (
+          <Grid key={item} item xs={12} sm={6} md={4} lg={3}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="/static/images/cards/contemplative-reptile.jpg"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    País
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Capital
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
+      <Pagination
+        color="secondary"
+        className={classes.pagination}
+        count={lista.length}
+        size="large"
+      />
     </Container>
   )
 }
